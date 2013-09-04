8d6ce949f1a6a5b39a9d93a15046e60fd0ceb89b
dot-star <zachborboa+dot-star@gmail.com>
2013-09-04 11:21:36 -0700

    Use macvim when available for vim aliases

--- .dotstar/vim/.aliases.sh
+++ .dotstar/vim/.aliases.sh
@@ -1,6 +1,13 @@
 # Vim aliases
 
-alias v="vim"
-alias vi="vim"
-alias vim="vim"
+if which "mvim" >/dev/null; then
+    alias mvim="open -a MacVim"
+    alias v="mvim"
+    alias vi="mvim"
+    alias vim="mvim"
+else
+    alias v="vim"
+    alias vi="vim"
+fi
+
 alias vimrc="vim ~/.vimrc"
