95e050654afa217fa396a46d652485f411bc9422
dot-star <zachborboa+dot-star@gmail.com>
2013-09-05 17:40:05 -0700

    Update any existing bootstrap during install

--- script/install.sh
+++ script/install.sh
@@ -6,7 +6,14 @@ set -e
 DOT_STAR_ROOT="$( dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ))"
 ln -sf "${DOT_STAR_ROOT}" "${HOME}"
 
-# Add bootstrap to bash profile.
-if ! grep --quiet "# .dotstar bootstrap" "$HOME/.bash_profile"; then
-    echo -e "\n# .dotstar bootstrap\n[[ -r ~/.dotstar/bash/.bash_profile ]] && . ~/.dotstar/bash/.bash_profile\n" >> "$HOME/.bash_profile"
+# Find existing bootstrap in bash profile.
+line_number=$(grep --line-number "# .dotstar bootstrap" "${HOME}/.bash_profile" | cut -d ":" -f "1")
+if [ ! -z "${line_number}" ]; then
+    # Remove installed bootstrap.
+    next_line_number="${line_number}"
+    (( next_line_number += 1 ))
+    sed -i "" "${line_number},${next_line_number}d" "${HOME}/.bash_profile"
 fi
+
+# Add bootstrap to bash profile.
+echo -e "# .dotstar bootstrap\n[[ -r ~/.dotstar/bash/.bash_profile ]] && . ~/.dotstar/bash/.bash_profile" >> "$HOME/.bash_profile"
