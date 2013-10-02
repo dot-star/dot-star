626f480aa6a29d9678fb038978b94301e3aa8b67
dot-star <zachborboa+dot-star@gmail.com>
2013-10-02 13:50:36 -0700

    Clean up vim aliases

--- vim/.aliases.sh
+++ vim/.aliases.sh
@@ -1,13 +1,13 @@
 # Vim aliases
 
+alias v="vim"
+alias vi="vim"
+
 if which "mvim" >/dev/null; then
     alias mvim="open -a MacVim"
     alias v="mvim"
     alias vi="mvim"
     alias vim="mvim"
-else
-    alias v="vim"
-    alias vi="vim"
 fi
 
 alias vimrc="vim ~/.vimrc"
