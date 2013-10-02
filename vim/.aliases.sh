f406b0a613ccce26c4a42b8a520bc04e1ba5de84
dot-star <zachborboa+dot-star@gmail.com>
2013-10-02 14:29:50 -0700

    Ask user to create files that don't exist when opening nonexistent files in vim

--- vim/.aliases.sh
+++ vim/.aliases.sh
@@ -1,13 +1,38 @@
 # Vim aliases
 
-alias v="vim"
-alias vi="vim"
+_vim () {
+    param_count="${#}"
+    if [ "${param_count}" -ge 1 ]; then
+        for param in "$@"; do
+            if [ ! -d "${param}" ]; then
+                file="${param}"
+                if [ ! -e "${file}" ]; then
+                    read -p "File \"${file}\" doesn't exist. Create file? " -n 1 -r
+                    echo
+                    if [[ $REPLY =~ ^[Yy]$ ]]; then
+                        touch "${file}"
+                    fi
+                fi
+            fi
+        done
+    fi
+
+    if which "mvim" >/dev/null; then
+        open -a MacVim "$@"
+    else
+        vim -p "$@"
+    fi
+}
+
+alias v="_vim"
+alias vi="_vim"
+alias vim="_vim"
 
 if which "mvim" >/dev/null; then
     alias mvim="open -a MacVim"
-    alias v="mvim"
-    alias vi="mvim"
-    alias vim="mvim"
+    alias v="_vim"
+    alias vi="_vim"
+    alias vim="_vim"
 fi
 
-alias vimrc="vim ~/.vimrc"
+alias vimrc="_vim ~/.vimrc"
