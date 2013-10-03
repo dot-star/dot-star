2c2362bff542eb00d5afe62521bc9bce51daf3da
dot-star <zachborboa+dot-star@gmail.com>
2013-10-03 10:34:13 -0700

    Add alias to dotstar root

--- bash/.aliases.sh
+++ bash/.aliases.sh
@@ -41,3 +41,5 @@ alias s="subl ."
 alias t="tree"
 
 alias ip="ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //' | sort | sed 's/\('$(ipconfig getifaddr en1)'\)/\1 [LOCAL]/'"
+
+alias dotstar="cd ${HOME}/.dot-star && l"
