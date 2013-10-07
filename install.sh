d8df2d9f63cc7c98fddaba8c7ac0bdbd50882e31
dot-star <zachborboa+dot-star@gmail.com>
2013-10-07 12:02:51 -0700

    Clean up debugging

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-#set -e
-#set -x
-
 [[ $(basename "${BASH_SOURCE}") = "install.sh" ]] && install=true || install=false
 [[ "${install}" = true ]] && update=false || update=true
-echo "install: ${install}"
-echo "update: ${update}"
 
 if $install; then
     echo -n "Installing..."
--- script/update.sh
+++ script/update.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -x
-
 git pull
 
 wget "https://raw.github.com/git/git/master/contrib/completion/git-completion.bash" -O git/git-completion.bash
