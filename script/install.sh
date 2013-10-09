63faba5faf989b298429bf932024808050731d4d
dot-star <zachborboa+dot-star@gmail.com>
2013-10-09 11:19:18 -0700

    Move bootstrap to bashrc so that dotstar is always initialized

--- script/install.sh
+++ script/install.sh
@@ -17,7 +17,19 @@ if [ ! -z "${line_number}" ]; then
 fi
 
 # Add bootstrap to bash profile.
-echo -e "# .dotstar bootstrap\n[[ -r ~/.dot-star/bash/.bash_profile ]] && . ~/.dot-star/bash/.bash_profile" >> "$HOME/.bash_profile"
+echo -e "# .dotstar bootstrap\n[[ -r ~/.bashrc ]] && source ~/.bashrc" >> "$HOME/.bash_profile"
+
+# Find existing bootstrap in bashrc.
+line_number=$(grep --line-number "# .dotstar bootstrap" "${HOME}/.bashrc" | cut -d ":" -f "1")
+if [ ! -z "${line_number}" ]; then
+    # Remove installed bootstrap.
+    next_line_number="${line_number}"
+    (( next_line_number += 1 ))
+    sed -i "" "${line_number},${next_line_number}d" "${HOME}/.bashrc"
+fi
+
+# Add bootstrap to bashrc.
+echo -e "# .dotstar bootstrap\n[[ -r ~/.dot-star/bash/.bash_profile ]] && source ~/.dot-star/bash/.bash_profile" >> "$HOME/.bashrc"
 
 # Run post installation script.
 source "${DOT_STAR_ROOT}/script/post_install.sh"
