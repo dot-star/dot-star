e5608eabb38a89106248c52c53c5b1bda5496f26
user52 <user52@darklaunch.com>
2013-10-11 12:40:09 -0700

    Add alias `devrunserver' to start runserver in development mode

--- django/.aliases.sh
+++ django/.aliases.sh
@@ -10,8 +10,21 @@ migrate() {
     ./manage.py migrate $@
 }
 
+_runserver() {
+    production="${1}"
+
+    if "${production}" 2> /dev/null; then
+        export PRODUCTION=1
+    else
+        export PRODUCTION=0
+    fi
+
+    python manage.py runserver 0.0.0.0:8000
+}
+
 alias dbshell="python manage.py dbshell"
-alias runserver="python manage.py runserver 0.0.0.0:8000"
+alias devrunserver="_runserver false"
+alias runserver="_runserver true"
 alias shell="python manage.py shell"
 alias startapp="python manage.py startapp"
 alias syncdb="python manage.py syncdb"
