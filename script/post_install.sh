5ee1048b8d5f1f770696f0a157b31f63c1f39421
dot-star <zachborboa+dot-star@gmail.com>
2013-10-14 12:17:38 -0700

    Install vimrc during post install

--- script/post_install.sh
+++ script/post_install.sh
@@ -1,3 +1,8 @@
 # Configure global gitignore.
 [[ ! -e "${HOME}/.gitignore" ]] && ln -vs "${DOT_STAR_ROOT}/git/.gitignore" "${HOME}"
 git config --global core.excludesfile "~/.gitignore"
+
+# Configure vimrc.
+if [ ! -e "${HOME}/.vimrc" ]; then
+    ln -s -v "${DOT_STAR_ROOT}/vim/.vimrc" "${HOME}"
+fi
