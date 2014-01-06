8f6029e2cb3ac919dc336a4d82eaa482e4880db8
Zach Borboa <zachborboa@gmail.com>
2014-01-06 09:31:14 -0800

    Fix sed string replacement on Ubuntu

--- script/install.sh
+++ script/install.sh
@@ -10,7 +10,10 @@ if [ ! -z "${line_number}" ]; then
     # Remove installed bootstrap.
     next_line_number="${line_number}"
     (( next_line_number += 1 ))
-    sed -i "" "${line_number},${next_line_number}d" "${HOME}/.bash_profile"
+    sed -i "" "${line_number},${next_line_number}d" "${HOME}/.bash_profile" &> /dev/null
+    if [ ! $? -eq 0 ]; then
+        sed --in-place="" "${line_number},${next_line_number}d" "${HOME}/.bash_profile"
+    fi
 fi
 
 # Add bootstrap to bash profile.
@@ -22,23 +25,29 @@ if [ ! -z "${line_number}" ]; then
     # Remove installed bootstrap.
     next_line_number="${line_number}"
     (( next_line_number += 1 ))
-    sed -i "" "${line_number},${next_line_number}d" "${HOME}/.bashrc"
+    sed -i "" "${line_number},${next_line_number}d" "${HOME}/.bashrc" &> /dev/null
+    if [ ! $? -eq 0 ]; then
+        sed --in-place="" "${line_number},${next_line_number}d" "${HOME}/.bashrc"
+    fi
 fi
 
 # Add bootstrap to bashrc.
 echo -e "# .dotstar bootstrap\n[[ -r ~/.dot-star/bash/.bash_profile ]] && source ~/.dot-star/bash/.bash_profile" >> "$HOME/.bashrc"
 
 # Find existing settings in inputrc.
-line_number=$(grep --line-number "# .dotstar inputrc" "${HOME}/.inputrc" | cut -d ":" -f "1")
+line_number=$(grep --line-number "# .dotstar" "${HOME}/.inputrc" | cut -d ":" -f "1")
 if [ ! -z "${line_number}" ]; then
     # Remove installed settings
     next_line_number="${line_number}"
     (( next_line_number += 1 ))
-    sed -i "" "${line_number},${next_line_number}d" "${HOME}/.inputrc"
+    sed -i "" "${line_number},${next_line_number}d" "${HOME}/.inputrc" &> /dev/null
+    if [ ! $? -eq 0 ]; then
+        sed --in-place="" "${line_number},${next_line_number}d" "${HOME}/.inputrc"
+    fi
 fi
 
 # Add settings to inputrc.
-echo -e "# .dotstar inputrc\nset completion-ignore-case on" >> "$HOME/.inputrc"
+echo -e "# .dotstar\nset completion-ignore-case on" >> "$HOME/.inputrc"
 
 # Run post installation script.
 source "${DOT_STAR_ROOT}/script/post_install.sh"
