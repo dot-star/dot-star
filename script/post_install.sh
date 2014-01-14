c90e501af217756819c2ce7f7c10210165b9f744
Zach Borboa <zachborboa@gmail.com>
2014-01-14 09:18:25 -0800

    Fix #24: Add vim theme railscat

--- script/post_install.sh
+++ script/post_install.sh
@@ -6,7 +6,13 @@ git config --global core.excludesfile "~/.gitignore"
 
 # Create backup and swap directories specified in vimrc.
 mkdir -p "$HOME/.vim/backup/"
+mkdir -p "$HOME/.vim/colors/"
 mkdir -p "$HOME/.vim/swap/"
+
+# Install vim theme.
+if [ ! -L "${HOME}/.vim/colors/railscat.vim" ]; then
+    ln -v -s "${DOT_STAR_ROOT}/vim/colors/railscat.vim" "${HOME}/.vim/colors/"
+fi
 
 # Configure vimrc.
 if [ ! -e "${HOME}/.vimrc" ]; then
