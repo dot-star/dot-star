114c018f425fc3853160f6c56d534dcb04fb0c9e
Zach Borboa <zachborboa@gmail.com>
2014-09-19 17:04:38 -0700

    Add alias q

--- bash/.aliases.sh
+++ bash/.aliases.sh
@@ -153,6 +153,7 @@ alias clean="sudo apt-get clean"
 alias distupgrade="sudo apt-get dist-upgrade"
 alias upgrade="sudo apt-get upgrade"
 alias x="exit"
+alias q="exit"
 alias reboot="sudo shutdown -r now"
 
 _open() {
