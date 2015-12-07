c13eaac50f03ea51d3796a2262f5da558eb68f22
Zach Borboa <zachborboa@gmail.com>
2015-12-07 01:33:00 -0800

    Remove hg and svn support

--- version_control/.aliases.sh
+++ version_control/.aliases.sh
@@ -119,90 +119,28 @@ is_git() {
     return 1
 }
 
-is_svn() {
-    svn log --limit=1 &> /dev/null
-    if [ $? -eq 0 ]; then
-        return 0
-    fi
-
-    # Look for a Subversion .svn folder in the current directory to indicate
-    # that we are in a Subversion repository. Avoid using `svn status' because
-    # it will erroneously return 0 (success) whilst declaring "svn: warning: '.'
-    # is not a working copy".
-    search=$(find . -maxdepth "1" -type "d" -name ".svn" -print -quit)
-    if [ ! -z "${search}" ]; then
-        return 0
-    fi
-
-    return 1
-}
-
-is_hg() {
-    hg log --limit=1 &> /dev/null
-    if [ $? -eq 0 ]; then
-        return 0
-    fi
-    return 1
-}
-
 rc_add() {
-    # git and hg support
     if is_git; then
         git add "$@"
-    elif is_hg; then
-        hg add "$@"
-    elif is_svn; then
-        echo "NotImplementedError"
     fi
 }
 
 rc_branch() {
-    # git and hg support
     if is_git; then
         git branch
-    elif is_hg; then
-        hg branch
-    elif is_svn; then
-        # TODO: svn ls svn://www.example.com/svn/branches/
-        # svn branch
-        echo "NotImplementedError"
     fi
 }
 
 rc_branches() {
-    # git and hg support
     if is_git; then
         git branch --all
-    elif is_hg; then
-        #hg branches | sort
-        current_branch=$(branch)
-        branch_list=$(hg branches | sort | cut -d " " -f 1)
-        for branch in ${branch_list}; do
-            active_branch=" "
-            if [ "${branch}" == "${current_branch}" ]; then
-                active_branch="*"
-            fi
-            line="${active_branch} ${branch}"
-            echo "${line}"
-        done
-    elif is_svn; then
-        echo "NotImplementedError"
     fi
 }
 
 rc_checkout() {
     # Revision Control checkout
-    # git and hg support
     if is_git; then
         git checkout $@
-    elif is_hg; then
-        hg checkout $@
-        return_code="${?}"
-        # Display modified files when `hg checkout' fails with "abort: crosses
-        # branches (merge branches or use --clean to discard changes)".
-        if [ "${return_code}" -eq 255 ]; then
-            hg status | \grep '^M '
-        fi
     else
         echo "NotImplementedError"
     fi
@@ -211,10 +149,6 @@ rc_checkout() {
 rc_commit() {
     if is_git; then
         git commit -m "$@"
-    elif is_hg; then
-        hg commit "$@"
-    elif is_svn; then
-        echo "NotImplementedError"
     fi
 }
 
@@ -248,30 +182,7 @@ rc_diff() {
             colordiff_installed=true
         fi
 
-        if is_svn; then
-            if ! $colordiff_installed ; then
-                echo -e '\x1b[0;93mWARNING\x1b[0m: colordiff does not seem to be installed.'
-            fi
-
-            if [ $# == 0 ]; then
-                svn diff . | colordiff | less -R
-            else
-                if [ $# == 1 ]; then
-                    svn diff "${1}" | colordiff | less -R
-                else
-                    svn diff $@ | colordiff | less -R
-                fi
-            fi
-        elif is_hg; then
-            if $colordiff_installed; then
-                hg diff --git . | colordiff | less --RAW-CONTROL-CHARS
-                echo "hg diff --git . | colordiff | less --RAW-CONTROL-CHARS"
-            else
-                echo -e '\x1b[0;93mWARNING\x1b[0m: colordiff does not seem to be installed.'
-                echo "hg diff --git ."
-                hg diff --git .
-            fi
-        elif is_g; then
+        if is_g; then
             p4 diff
         else
             \df $@
@@ -282,20 +193,15 @@ rc_diff() {
 rc_log() {
     if is_git; then
         git log "${@}"
-    elif is_hg; then
-        hg log | less
     else
         echo "NotImplementedError"
     fi
 }
 
 rc_master() {
-    # Revision Control master / default (hg)
-    # git and hg support
+    # Revision Control master.
     if is_git; then
         git checkout master
-    elif is_hg; then
-        hg checkout default
     else
         echo "NotImplementedError"
     fi
@@ -305,8 +211,6 @@ rc_merge() {
     # Revision Control merge.
     if is_git; then
         git merge $@
-    elif is_hg; then
-        hg merge $@
     else
         \merge $@
     fi
@@ -314,11 +218,8 @@ rc_merge() {
 
 rc_pull() {
     # Revision Control pull
-    # git and hg support
     if is_git; then
         git pull $@
-    elif is_hg; then
-        hg pull $@
     else
         echo "NotImplementedError"
     fi
@@ -326,11 +227,8 @@ rc_pull() {
 
 rc_push() {
     # Revision Control push
-    # git and hg support
     if is_git; then
         git push $@
-    elif is_hg; then
-        hg push --new-branch $@
     else
         echo "NotImplementedError"
     fi
@@ -338,27 +236,17 @@ rc_push() {
 
 rc_status() {
     # Revision Control status
-    # git, hg, and svn support
-
     clear
 
     # If params empty, retrieve status of current directory
     params=$@
     if [ -z "$params" ]; then
-        if ! is_hg; then
-            params="."
-        fi
+        params="."
     fi
 
     if is_git; then
         echo "git status $params"
         git status $params
-    elif is_hg; then
-        echo "hg status $params"
-        hg status $params
-    elif is_svn; then
-        echo "svn status $params"
-        svn status $params
     elif is_g; then
         echo "g4 pending"
         g4 pending
