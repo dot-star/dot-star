5ab83e51043ca38fe54bc61c7abb417ade3c55c0
Zach Borboa <zachborboa@gmail.com>
2018-10-03 22:40:51 -0700

    Move history-search-* bindings to enable them when fzf is in use

--- bash/.behavior.sh
+++ bash/.behavior.sh
@@ -9,3 +9,18 @@ shopt -s nocaseglob
 
 # Type only the directory name to change into the directory.
 shopt -s autocd
+
+# Use the text that has been typed as the prefix for searching up and down
+# through commands in the history.
+#
+# Use the `bind' command here to enable this behaviour instead of specifying
+# inside .inputrc as fzf causes the setting to be ignored.
+#   .inputrc
+#   "\e[B": history-search-forward
+#   "\e[A": history-search-backward
+#
+#   $ bind -P | grep "history-search-"
+#   history-search-backward is not bound to any keys
+#   history-search-forward is not bound to any keys
+bind '"\e[A": history-search-backward'
+bind '"\e[B": history-search-forward'
