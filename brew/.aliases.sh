afbdf12aa60ab93242114070c02859fb00b5141a
Zach Borboa <zachborboa@gmail.com>
2020-04-07 09:57:41 -0700

    Fix grep option --dereference-recursive unavailable.
    Error message:
        $ se $keyword
        grep: unrecognized option `--dereference-recursive'
        usage: grep [-abcDEFGHhIiJLlmnOoqRSsUVvwxZ] [-A num] [-B num] [-C[num]]
        	[-e pattern] [-f file] [--binary-files=value] [--color=when]
        	[--context[=num]] [--directories=action] [--label] [--line-buffered]
        	[--null] [pattern] [file ...]

--- brew/.aliases.sh
+++ brew/.aliases.sh
@@ -1,9 +1,13 @@
+# Make commands available without needing the "g" prefix (e.g. gls -> ls, ggrep -> grep).
 # Use coreutils from brew. Install via `$ brew install coreutils'.
+# Use grep from brew. Install via `$ brew install grep'.
 which brew &> /dev/null
 if [[ $? -eq 0 ]]; then
     # Use string path as this brew command is slow.
     # coreutils_path=$(brew --prefix coreutils)
     coreutils_path="/usr/local/opt/coreutils"
+
     export PATH="${coreutils_path}/libexec/gnubin:${PATH}"
+    export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
     export MANPATH="${coreutils_path}/libexec/gnuman:${MANPATH}"
 fi
