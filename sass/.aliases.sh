29e77583795513505424a4dd647d47136d1d6f99
Zach Borboa <zachborboa@gmail.com>
2020-09-02 12:24:20 -0700

    Allow setting a file name for the source and resulting stylesheet

--- sass/.aliases.sh
+++ sass/.aliases.sh
@@ -1,9 +1,24 @@
 sasswatch() {
-    param_count="${#}"
-    command="style.scss:style.css"
+    # Usage:
+    #   $ sasswatch
+    #   sass [...] --watch "style.scss:style.css"
+    #
+    #   $ sasswatch my_style
+    #   sass [...] --watch "my_style.scss:my_style.css"
+    #
+    #   $ sasswatch my_style.css
+    #   sass [...] --watch "my_style.scss:my_style.css"
+    #
+    #   $ sasswatch my_style.scss
+    #   sass [...] --watch "my_style.scss:my_style.css"
 
-    if [ "${param_count}" -ge 1 ]; then
-        command="${@}"
+    if [[ "${#}" -eq 1 ]]; then
+        arg="${1}"
+        arg="${arg/.css/}"
+        arg="${arg/.scss/}"
+        command="${arg}.scss:${arg}.css"
+    else
+        command="style.scss:style.css"
     fi
 
     sass \
