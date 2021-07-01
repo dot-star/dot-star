f4acea7ff247e8c20b7ee35ab619f7bddbbe77da
Zach Borboa <zachborboa@gmail.com>
2021-07-01 19:52:30 -0400

    Save commands to history after execution

--- bash/.history.sh
+++ bash/.history.sh
@@ -4,3 +4,6 @@ export HISTCONTROL=ignoredups:ignorespace
 # Increase size of bash history.
 export HISTSIZE=32768
 export HISTFILESIZE="${HISTSIZE}"
+
+# Save each command in bash history right after it has been executed.
+PROMPT_COMMAND='history -a'
