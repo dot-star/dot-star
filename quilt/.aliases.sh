552050164a999e36135512b0ec115fd14f045d2b
Zach Borboa <zachborboa@gmail.com>
2021-08-25 14:43:59 -0400

    Add alias helper for running `quilt new'

--- quilt/.aliases.sh
+++ quilt/.aliases.sh
@@ -3,3 +3,27 @@ alias refresh="quilt refresh"
 alias ser="quilt series"
 alias series="quilt series"
 alias to="quilt top"
+
+_quilt_new() {
+    # Usage:
+    #   $ new changes.diff
+    #   quilt new changes.diff
+    #
+    #   $ new changes
+    #   quilt new changes.diff
+    #
+    #   $ new my changes
+    #   quilt new my-changes.diff
+
+    file_name="${@}"
+    file_name="$(echo "${file_name}" | slugify)"
+
+    if [[ "${file_name}" != *".diff" ]]; then
+        file_name="${file_name}.diff"
+    fi
+
+    set -x
+    quilt new "${file_name}"
+    set +x
+}
+alias new="_quilt_new"
