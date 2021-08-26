3bd55831b953303ed37a8e3671b692aab131f8b4
Zach Borboa <zachborboa@gmail.com>
2021-08-25 22:52:47 -0400

    Implement a basic "quilt mode"

--- quilt/.aliases.sh
+++ quilt/.aliases.sh
@@ -1,9 +1,5 @@
-alias re="quilt refresh"
-alias ref="quilt refresh"
-alias refresh="quilt refresh"
-alias ser="quilt series"
-alias series="quilt series"
-alias to="quilt top"
+alias qu="_quilt"
+alias quilt="_quilt"
 
 _quilt_new() {
     # Usage:
@@ -27,4 +23,69 @@ _quilt_new() {
     quilt new "${file_name}"
     set +x
 }
-alias new="_quilt_new"
+
+_quilt_set_alias() {
+    existing_alias_to_override="${1}"
+    new_alias_value="${2}"
+
+    if type "${existing_alias_to_override}" &> /dev/null; then
+        unalias "${existing_alias_to_override}"
+    fi
+
+    alias "${existing_alias_to_override}"="${new_alias_value}"
+}
+
+_quilt_override_aliases() {
+    _quilt_set_alias "n"   "_quilt_new"
+    _quilt_set_alias "new" "_quilt_new"
+
+    _quilt_set_alias "po"  "quilt pop"
+    _quilt_set_alias "pop" "quilt pop"
+
+    _quilt_set_alias "pu"   "quilt push"
+    _quilt_set_alias "push" "quilt push"
+
+    _quilt_set_alias "q" "quilt"
+
+    _quilt_set_alias "r"       "quilt refresh"
+    _quilt_set_alias "re"      "quilt refresh"
+    _quilt_set_alias "ref"     "quilt refresh"
+    _quilt_set_alias "refresh" "quilt refresh"
+
+    _quilt_set_alias "se"     "quilt series"
+    _quilt_set_alias "ser"    "quilt series"
+    _quilt_set_alias "series" "quilt series"
+
+    _quilt_set_alias "t"   "quilt top"
+    _quilt_set_alias "to"  "quilt top"
+    _quilt_set_alias "top" "quilt top"
+}
+
+_quilt_activate_quilt_mode() {
+    export _QUILT_MODE_ON="yes"
+    export _OLD_QUILT_PS1="${PS1}"
+    export PS1="(quilt) ${PS1}"
+
+    bash
+    _quilt_deactivate_quilt_mode
+}
+
+_quilt_deactivate_quilt_mode() {
+    if [ -n "${_OLD_QUILT_PS1}" ]; then
+        export PS1="${_OLD_QUILT_PS1}"
+        unset _OLD_QUILT_PS1
+        unset _QUILT_MODE_ON
+    fi
+}
+
+_quilt() {
+    if [ -z "${_QUILT_MODE_ON}" ]; then
+        _quilt_activate_quilt_mode
+    else
+        \quilt "${@}"
+    fi
+}
+
+if [ -n "${_QUILT_MODE_ON}" ]; then
+    _quilt_override_aliases
+fi
