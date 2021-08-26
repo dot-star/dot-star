195e879175293e2071a16c7ae76e35d203ce6615
Zach Borboa <zachborboa@gmail.com>
2021-08-25 20:02:03 -0400

    Fix PS1 "command not found" issue when running new bash shell
    Example:
        $ bash
        bash: prompt_git: command not found

--- bash/.prompt.sh
+++ bash/.prompt.sh
@@ -110,7 +110,8 @@ PS1+="\[${white}\] at ";
 PS1+="\[${hostStyle}\]\h"; # host
 PS1+="\[${white}\] in ";
 PS1+="\[${green}\]\w"; # working directory full path
-PS1+="\$(prompt_git \"\[${white}\] on \[${violet}\]\" \"\[${blue}\]\")"; # Git repository details
+#PS1+="\$(prompt_git \"\[${white}\] on \[${violet}\]\" \"\[${blue}\]\")"; # Git repository details
+PS1+="\$(type prompt_git &> /dev/null && prompt_git \"\[${white}\] on \[${violet}\]\" \"\[${blue}\]\")"; # Git repository details
 PS1+="\n";
 PS1+="\[${white}\]\$ \[${reset}\]"; # `$` (and reset color)
 export PS1;
