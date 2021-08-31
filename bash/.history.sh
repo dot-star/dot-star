77856b43632e2a9b84c1e96ef5895cca5fe1c6fc
Zach Borboa <zachborboa@gmail.com>
2021-08-30 21:39:01 -0400

    Fix PROMPT_COMMAND causing new tabs to not retain the same working directory

--- bash/.history.sh
+++ bash/.history.sh
@@ -6,4 +6,8 @@ export HISTSIZE=32768
 export HISTFILESIZE="${HISTSIZE}"
 
 # Save each command in bash history right after it has been executed.
-PROMPT_COMMAND='history -a'
+# Append the PROMPT_COMMAND variable (e.g. "; $PROMPT_COMMAND") after modifying
+# PROMPT_COMMAND to avoid breaking PROMPT_COMMAND. Without doing this, new tabs
+# won't retrain the current directory even with the Preferences > General > New
+# tabs open with: Same Working Directory setting set.
+PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
