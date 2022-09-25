a8c3412721734bba141224a598e2fe1ac0fe83c5
Zach Borboa <zachborboa@gmail.com>
2022-09-24 18:18:56 -0700

    Use fzf selector for sasswatch command

--- sass/.aliases.sh
+++ sass/.aliases.sh
@@ -3,33 +3,41 @@ sasswatch() {
     #   $ sasswatch
     #   sass [...] --watch "style.scss:style.css"
     #
-    #   $ sasswatch "style.scss:style.css"
+    #   $ sasswatch "style.scss"
     #   sass [...] --watch "style.scss:style.css"
-    #
-    #   $ sasswatch my_style
-    #   sass [...] --watch "my_style.scss:my_style.css"
-    #
-    #   $ sasswatch my_style.css
-    #   sass [...] --watch "my_style.scss:my_style.css"
     #
     #   $ sasswatch my_style.scss
     #   sass [...] --watch "my_style.scss:my_style.css"
 
-    local command
+    local input=""
+    local output=""
+
     if [[ "${#}" -eq 0 ]]; then
-        command="style.scss:style.css"
-    elif [[ "${#}" -eq 1 ]] && [[ "${1}" == *":"* ]]; then
-        command="${1}"
-    elif [[ "${#}" -eq 1 ]] && [[ "${1}" != *":"* ]]; then
-        arg="${1}"
-        arg="${arg/.css/}"
-        arg="${arg/.scss/}"
-        command="${arg}.scss:${arg}.css"
+        input="$(find . -type f -name "*.scss" | fzf)"
+    elif [[ "${#}" -eq 1 ]]; then
+        input="${1}"
     fi
 
-    sass \
-        --no-source-map \
-        --style="expanded" \
-        --watch \
-        "${command}"
+    if [[ -z "${output}" ]] && [[ ! -z "${input}" ]]; then
+        output="${input/.scss/}.css"
+    fi
+
+    if [[ ! -z "${input}" ]]; then
+        echo "input: ${input}"
+    fi
+
+    if [[ ! -z "${output}" ]]; then
+        echo "output: ${output}"
+    fi
+
+    if [[ ! -z "${input}" ]] && [[ ! -z "${output}" ]]; then
+        set -x
+        sass \
+            --no-source-map \
+            --style="expanded" \
+            --watch \
+            "${input}:${output}"
+        set +x
+    fi
 }
+alias sw="sasswatch"
