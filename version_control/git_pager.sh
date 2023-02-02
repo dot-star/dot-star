1909800c9fa97ddce52becd95dc700f258ac1bb4
Zach Borboa <zachborboa@gmail.com>
2023-02-02 14:57:19 -0800

    Add compatibility when brew prefix is not /usr/local

--- version_control/git_pager.sh
+++ version_control/git_pager.sh
@@ -1,1 +1,6 @@
-"/usr/local/Cellar/git/"*"/share/git-core/contrib/diff-highlight/diff-highlight" | less -m
+homebrew_prefix="${HOMEBREW_PREFIX}"
+if [[ -z "${HOMEBREW_PREFIX}" ]]; then
+    homebrew_prefix="/usr/local"
+fi
+
+"${homebrew_prefix}/Cellar/git/"*"/share/git-core/contrib/diff-highlight/diff-highlight" | less -m
