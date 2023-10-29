3a706cd21cd68b6947d14522e583e84596382c6b
Zach Borboa <zachborboa@gmail.com>
2023-10-28 18:35:52 -0700

    Use command line option name

--- version_control/git_pager.sh
+++ version_control/git_pager.sh
@@ -3,4 +3,4 @@ if [[ -z "${HOMEBREW_PREFIX}" ]]; then
     homebrew_prefix="/usr/local"
 fi
 
-"${homebrew_prefix}/Cellar/git/"*"/share/git-core/contrib/diff-highlight/diff-highlight" | less -m
+"${homebrew_prefix}/Cellar/git/"*"/share/git-core/contrib/diff-highlight/diff-highlight" | less --long-prompt
