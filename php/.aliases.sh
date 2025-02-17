6c167d1b03f737c6a6f29bdeed649df5419744d9
Zach Borboa <zachborboa@gmail.com>
2025-02-16 21:27:14 -0500

    Add php switcher

---
+++ php/.aliases.sh
@@ -0,0 +1,15 @@
+php83_switch_to() {
+    echo "current version:"
+    php -v
+    which php
+    echo
+
+    echo "switching to php 8.3"
+    brew install php@8.3
+    brew link --overwrite php@8.3
+    echo
+
+    echo "current version is now:"
+    php -v
+    which php
+}
