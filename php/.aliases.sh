03d885b880f5184ae2781cf546b98b1c54dc50aa
Zach Borboa <zachborboa@gmail.com>
2025-02-17 19:03:37 -0500

    Add PHP 8.5 switch alias

--- php/.aliases.sh
+++ php/.aliases.sh
@@ -13,3 +13,19 @@ php83_switch_to() {
     php -v
     which php
 }
+
+php85_switch_to() {
+    echo "current version:"
+    php -v
+    which php
+    echo
+
+    echo "switching to php 8.5"
+    brew install php@8.5
+    brew link --overwrite php@8.5
+    echo
+
+    echo "current version is now:"
+    php -v
+    which php
+}
