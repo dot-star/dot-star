ae4594a59b7f9a383331b928bc1ea22b665e0311
Zach Borboa <zachborboa@gmail.com>
2025-03-29 20:07:08 +0000

    Exclude the chunks folder when searching (NextJS)

--- bash/.aliases.sh
+++ bash/.aliases.sh
@@ -463,6 +463,7 @@ alias_grep() {
             --exclude-dir=".hg" \
             --exclude-dir=".pytest_cache" \
             --exclude-dir=".svn" \
+            --exclude-dir="chunks" \
             --exclude-dir="node_modules" \
             --exclude-dir="vendor" \
             --exclude=".phpunit.result.cache" \
