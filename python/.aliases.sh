c24d14dee3dae83367ddbacd589d672a2d338332
Zach Borboa <zachborboa@gmail.com>
2025-12-23 09:52:02 -0400

    Add aliases for updating pip

--- python/.aliases.sh
+++ python/.aliases.sh
@@ -5,3 +5,21 @@ python_version() {
     which python3
 }
 alias pyv="python_version"
+
+update_pip() {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        set -x
+        brew upgrade python
+        set +x
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        set -x
+        python3 -m pip install --upgrade pip
+        set +x
+    else
+        echo "Unsupported OS type: ${OSTYPE}"
+        return 1
+    fi
+}
+alias pipup="update_pip"
+alias upip="update_pip"
+alias uppip="update_pip"
