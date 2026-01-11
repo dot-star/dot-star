70b96bd49df852c2e43667178b28455dcd98c11b
Zach Borboa <zachborboa@gmail.com>
2026-01-11 13:57:09 -0500

    Add lazy load for node

--- node/.aliases.sh
+++ node/.aliases.sh
@@ -1,13 +1,26 @@
+alias_node() {
+    # Lazy-load nvm when node is called.
+
+    unalias node
+    unalias npx
+
+    # ...because this is slow:
+    [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+
+    node "${@}"
+}
+
 alias_npx() {
     # Lazy-load nvm when npx is called.
 
-    # type npx
     unalias npx
-    # type npx
+    unalias node
 
     # ...because this is slow:
     [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 
     npx "${@}"
 }
+
+alias node="alias_node"
 alias npx="alias_npx"
