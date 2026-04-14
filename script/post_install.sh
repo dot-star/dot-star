ec931f152b6d3e9572f0ebfd3b2372403ff162ee
Zach Borboa <zachborboa@gmail.com>
2026-04-14 10:11:21 -0400

    Skip confirmations

--- script/post_install.sh
+++ script/post_install.sh
@@ -60,7 +60,7 @@ if [[ "${OSTYPE}" == "darwin"* ]]; then
 
     # Install command-line fuzzy finder with key bindings and fuzzy completion.
     brew install fzf
-    $(brew --prefix)/opt/fzf/install
+    $(brew --prefix)/opt/fzf/install --all
 
     # Disable chime sound when power is connected.
     defaults write com.apple.PowerChime ChimeOnNoHardware -bool true
