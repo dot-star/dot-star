5787d8a08a5f6c0c197fa5e9b768432891bb2ce2
Zach Borboa <zachborboa@gmail.com>
2026-04-23 22:54:51 -0400

    Add clcm alias for generating commit messages like this one 😉

--- ai/.aliases.sh
+++ ai/.aliases.sh
@@ -2,3 +2,4 @@ export DISABLE_TELEMETRY=1
 
 alias cl="claude"
 alias clr="claude --resume"
+alias clcm="claude --print --output-format=json 'give me a one liner commit message for the staged changes' | \jq -r .result"
