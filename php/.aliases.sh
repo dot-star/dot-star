460eaf45f15566667435d78aa20328318bd68da5
Zach Borboa <zachborboa@gmail.com>
2026-05-12 09:55:34 -0400

    Remove `php74_switch_to` (7.4 is EOL, not in Homebrew core)

--- php/.aliases.sh
+++ php/.aliases.sh
@@ -1,19 +1,3 @@
-php74_switch_to() {
-    echo "current version:"
-    php -v
-    which php
-    echo
-
-    echo "switching to php 7.4"
-    brew install php@7.4
-    brew link --overwrite php@7.4
-    echo
-
-    echo "current version is now:"
-    php -v
-    which php
-}
-
 php81_switch_to() {
     echo "current version:"
     php -v
